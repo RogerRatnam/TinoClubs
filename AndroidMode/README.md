ac3af75e0f84494c0414cda086ddce354d567c5a
Vybhav B <vybs2499@gmail.com>
2016-02-05 19:56:41 -0800

    Had to delete files

--- AndroidMode/README.md
+++
@@ -1,2 +0,0 @@
-# TinoClubs
-Information of all clubs on campus
---
+++ test/V3/TinoClubsV3/app/src/main/java/com/vybhavb/tinoclubsv3/MemberView.java
@@ -0,0 +1,13 @@
+package com.vybhavb.tinoclubsv3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_view);
+    }
+}
